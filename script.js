c011a76c501a7c1bd6315ed4b4323cb93a3c9974
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-22 03:29:27 -0700

    Logical Operators in JS

---
+++ script.js
@@ -0,0 +1,10 @@
+// Just like Comparisions operators in JS there are also some Logical Operators that Work on Logic
+// AND &&  , OR ||  , NOT !
+// Example of AND 
+
+if (true || true){
+    console.log("hello")
+}
+else {
+    console.log("Hello1")
+}
