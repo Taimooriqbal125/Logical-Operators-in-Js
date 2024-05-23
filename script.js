370ece377b9f3a418a107d876bd677658993ca26
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-23 02:37:24 -0700

    Logical Operator in js

--- script.js
+++ script.js
@@ -2,9 +2,30 @@
 // AND &&  , OR ||  , NOT !
 // Example of AND 
 
-if (true || true){
-    console.log("hello")
+// if (true || true){
+//     console.log("hello")
+// }
+// else {
+//     console.log("Hello1")
+// }
+
+// if (true || false){
+//     console.log("hello 02")
+// }
+// else {
+//     console.log("nooutput")
+// }
+
+//  Use logicL OPERATOR IN ELSEIF STATEMENT
+if(true&&true){
+    console.log("yes")
 }
-else {
-    console.log("Hello1")
+else if (true&&false){
+    console.log("no")
+}
+else if (true && true){
+    connsole.log("yes")
+}
+else{
+    console.log("no result")
 }
